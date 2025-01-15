35ae81e0774cb5efa48cd9bc561d199b675399f1
Ernest <ernests.stals@gmail.com>
2025-01-15 17:12:26 +0100

    updated page with new component and logic to pass id

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,22 @@
-// import CompanySearch from '@/components/CompanySearch';
-
+"use client";
+import SimilarCompanies from "@/components/SimilarCompanies";
 import CompanySearch from "../components/CompanySearch";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedCompanyId, setSelectedCompanyId] = useState<string | null>(
+    null
+  );
+
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Company Search</h1>
-      <CompanySearch />
+      <CompanySearch onCompanySelect={(id) => setSelectedCompanyId(id)} />
+      {selectedCompanyId && (
+        <div className="mt-8">
+          <SimilarCompanies companyId={selectedCompanyId} itemCount={6} />
+        </div>
+      )}
     </main>
   );
 }
